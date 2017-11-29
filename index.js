68744ae64022f0c38719fddf0a69fc8e2a11e35a
bethtechow <bethtechow@yahoo.com>
2017-11-29 23:34:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ function scuberGreetingForFeet(distance){
         return ("No can do.")}
 }
 function ternaryCheckCity(response){
-  response === "NYC" ? "OK, sounds good." : "No go."
+  return response === "NYC" ? "OK, sounds good." : "No go."
 }
