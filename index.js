3825279843206dea2573d1e723ebee6d9419eb2c
bethtechow <bethtechow@yahoo.com>
2017-11-29 23:26:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code in this file!
+
+function scuberGreetingForFeet(distance){
+    if (distance <= 400) {
+      return ("This one is on me!")
+      }
+      if (distance <=2001) {
+        return ("I will gladly take your thirty bucks.")
+      }
+      if (distance >=2501) {
+        return ("No can do.")}
+}
+function ternaryCheckCity(response){
+  city = "NYC" ? "OK, sounds good." : "No go."
+}
