5d2e5e480d39408d92ecef123ebd34a4b4be2714
bethtechow <bethtechow@yahoo.com>
2017-11-30 00:08:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,5 +20,8 @@ function switchOnCharmFromTip(tipamount){
     case "generous":
     return "Thank you so much."
     break;
+    case "not as generous":
+    return "Thank you."
+    break;
   }
 }
